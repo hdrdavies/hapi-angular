52340fa5ba2cc07467fba29ae74a973f11c8be35
Huw <hdrdavies@gmail.com>
2016-02-26 11:08:00 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var Hapi   = require('hapi');
+var Inert  = require('inert');
+var Index  = require('./index.html');
+
+var server = new Hapi.Server();
+
+server.connection({
+  port:3000
+});
+
+server.register(Inert, function(err){
+  if (err) {
+    throw err;
+  }
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, reply) {
+      reply.file('./index.html');
+    }
+  });
+});
+
+
+server.start(function(err){
+  console.log('Server running at: ', server.info.uri);
+});
